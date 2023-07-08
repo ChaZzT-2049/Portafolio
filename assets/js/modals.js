30248006e3022bcfab60f2508e485fc59ef097d4
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2023-07-07 20:38:38 -0500

    modificacion a la logica de los modales de skill y experiencia

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,23 +1,44 @@
+//onclick="showModalXp(`pro-cv`)"
+const modalsk = document.getElementById("modalsk");
+const skillsModal = document.getElementsByClassName("modal__skill");
 
-const modal = document.getElementById("modal");
+const modalxp  = document.getElementById("modalxp")
+const proyectosModal = document.getElementsByClassName("modal__xp");
 
-function showModal(id){
-    displaySkillContent(id);
-    modal.style.display = "block";
-};
-function closeModal(){
-    modal.style.display = "none";
+const btnMas = document.getElementsByClassName("experience__mas");
+for(i = 0; i < btnMas.length; i++){
+    let id = btnMas[i].id
+    btnMas[i].addEventListener("click", function(){showModalXp(id)});
+}
+const skillBoxes = document.getElementsByClassName("skill__box");
+for(i = 0; i < skillBoxes.length; i++){
+    let id = skillBoxes[i].id
+    skillBoxes[i].addEventListener("click", function(){showModalSk(id)});
 }
 
-function displaySkillContent(id){
-    const skillsModal = document.getElementsByClassName("modal__skill");
+function showModalSk(id){
     for(i = 0; i < skillsModal.length; i++){
         if(skillsModal[i].id == id){
             skillsModal[i].style.display = "block";
-        }
-        else{
+        }else{
             skillsModal[i].style.display = "none";
-            closeModal;
         }
     }
+    modalsk.style.display = "block";
+}
+
+function showModalXp(id){
+    for(i = 0; i < proyectosModal.length; i++){
+        if(proyectosModal[i].id == id){
+            proyectosModal[i].style.display = "block";
+        }else{
+            proyectosModal[i].style.display = "none";
+        }
+    }
+    modalxp.style.display = "block";
+}
+
+function closeModal(){
+    modalsk.style.display = "none";
+    modalxp.style.display = "none";
 }
